48af59f5f8b35349befb8f5399ccd38a5a76bff3
Monica Coira <monicacoira@meta.com>
2025-06-11 15:22:33 -0700

    removed duplicates when using sort

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -2,14 +2,25 @@ import MovieCard from "./MovieCard";
 import './MovieList.css'
 
 function MovieList({data, sortChosenItem}){
+    function removeDuplicates(data){
+        return (data.filter((value, index, self) =>
+            index === self.findIndex((i) => (
+                i.id === value.id
+            ))
+        ));
+    }
+
     if (sortChosenItem === "title"){
         data = [...data].sort((a,b) => a.original_title.localeCompare(b.original_title));
+        data = removeDuplicates(data);
     }
     else if (sortChosenItem === "releaseDate"){
         data = [...data].sort((a,b) => new Date(b.release_date) - new Date(a.release_date));
+        data = removeDuplicates(data);
     }
     else if (sortChosenItem === "voteAverage"){
         data = [...data].sort((a,b) => b.vote_average - a.vote_average);
+        data = removeDuplicates(data);
     }
 
     return (
