89230d4b0b7d6726ad5a64f02aec8af048bb6fd3
Monica Coira <monicacoira@meta.com>
2025-06-13 15:00:24 -0700

    added placeholder movie images

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,9 +5,11 @@ import heartRed from "/src/assets/heartRed.png";
 import heartEmpty from "/src/assets/heartEmpty.png";
 import eye from "/src/assets/eye.png";
 import eyeSlash from "/src/assets/eye-slash.png";
+import moviePlaceholder from "/src/assets/movie.png";
 
 function MovieCard({image, title, rating, id, onCardClick}) {
     const movieImageUrl = `https://image.tmdb.org/t/p/w500${image}`;
+    const placeholderImage = moviePlaceholder;
     const [favoriteButton, setFavoriteButton] = useState(false)
     const [favoriteButtonUrl, setFavoriteButtonUrl] = useState(heartEmpty)
     const [movieIsWatched, setMovieIsWatched] = useState(false)
@@ -35,11 +37,21 @@ function MovieCard({image, title, rating, id, onCardClick}) {
         }
     }
 
+    const handleMovieImage = (image) => {
+        if (image === undefined || image === null){
+            return placeholderImage;
+        }
+        else {
+            return movieImageUrl;
+        }
+    }
+
     return (
         <>
+        {handleMovieImage}
         <div className="movie-card" onClick={onCardClick}>
             <div className="movie-card-id">{id}</div>
-            <img className="movie-card-image" src={movieImageUrl} alt={title} />
+            <img className="movie-card-image" src={handleMovieImage(image)} alt={title} />
             <h3 className="movie-card-title">{title}</h3>
             <p className="movie-card-rating">Rating: {rating}</p>
             <div className="movie-card-buttons">
